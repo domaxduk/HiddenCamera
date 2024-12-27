d5726d858c979991dcca384a5c3a9c368acc7c7c
domaxduk <domaxduk@gmail.com>
2024-12-27 14:41:18 +0700

    Initial Commit

---
+++ HiddenCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HiddenCamera
+//
+//  Created by Duc apple  on 27/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
